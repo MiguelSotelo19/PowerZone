ef3441e76371371703fb57ebdd7955588610b859
MiguelSotelo19 <133397326+MiguelSotelo19@users.noreply.github.com>
2024-11-13 22:46:33 -0600

    Commit antes de merge

--- power_zone/src/main.jsx
+++ power_zone/src/main.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 
 //Páginas
 import App from './Common/App.jsx'
