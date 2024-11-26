579cb3f429ae1c1bbf7792e186296603819945d7
DiegoVaz <vazquezsotelodiego@gmail.com>
2024-11-26 00:36:11 -0600

    Corrección error en menús y añadido alerta en Acceso

--- power_zone/src/Empleado/etiquetas/Menu.jsx
+++ power_zone/src/Empleado/etiquetas/Menu.jsx
@@ -40,7 +40,7 @@ function Menu() {
             <Nav.Link className="m-1 me-2 fs-5 fw-bold" href="/PowerZone/E/Clases">Clases</Nav.Link>
             <Nav.Link className="m-1 me-2 fs-5 fw-bold" href="/PowerZone/E/Membresias">Membresías</Nav.Link>
     
-            <Navbar.Brand href="/PowerZone/E/Perfil">
+            <Navbar.Brand>
             <Dropdown align="end" className="e-caret-hide">
                 <Dropdown.Toggle
                   variant="link"
