79cb3f56c479b742ff664b5d5c3a0ba4576d5169
DiegoVaz <vazquezsotelodiego@gmail.com>
2024-11-27 01:05:27 -0600

    Avance membresia de cliente

--- power_zone/src/Cliente/Membresia.jsx
+++ power_zone/src/Cliente/Membresia.jsx
@@ -12,90 +12,7 @@ import { Col, Form, Row, Table } from "react-bootstrap";
 import check from '../Common/img/check.png'
 import equis from '../Common/img/equis.png'
 
-function ModalMejorar({ show, onHide, datosCliente,datosMembresia }) {
-    return (
-        <Modal
-          show={show}
-          onHide={onHide}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Cambiar nivel de membresía
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h5>Nivel actual de membresía: {datosMembresia.tipo_membresia}</h5>
-            <Container>
-              <Row className="d-flex justify-content-center mt-3">
-                <Col md={12}>
-                      <Form>
-                          <Form.Group className="mb-3">
-                              <Form.Label>Número de cuenta:</Form.Label>
-                              <Form.Control type="number" placeholder="Número de cuenta (16 digitos)" 
-                              onInput={(e) => e.target.value = e.target.value.slice(0, 16)} required  />
-                              
-                          </Form.Group>
-                      </Form>
-                  </Col>
-              </Row>
-              <Row className="d-flex justify-content-center">
-                  <Col md={6}>
-                      <Form>
-                          <Form.Group className="mb-3">
-                              <Form.Label>Tipo de membresía:</Form.Label>
-                              <Form.Select type="text" placeholder="Nombre del cliente">
-                                <option value="Plus">Plus</option>
-                                <option value="Medium">Medium</option>
-                                <option value="Estandar">Estándar</option>
-                              </Form.Select>
-                              
-                          </Form.Group>
 
-                          <Form.Group className="mb-3">
-                              <Form.Label>Tipo de tarjeta:</Form.Label>
-                              <Form.Control type="number" placeholder="1112223333"
-                              />
-                          </Form.Group>
-                      </Form>
-                  </Col>
-
-                  <Col md={6}>
-                      <Form>
-                          <Form.Group className="mb-3">
-                              <Form.Label>CVV:</Form.Label>
-                              <Form.Control type="number" inputMode="number" placeholder="123"/>
-                          </Form.Group>
-
-                          <Form.Group className="mb-3">
-                              <Form.Label>Fecha de vencimiento:</Form.Label><br/>
-                              <Form.Control type="date"></Form.Control>
-                          </Form.Group>
-                      </Form>
-                  </Col>
-                </Row>
-
-            </Container>
-            {/*tengo planeado poner el <Planes/> aqui, pero sale con los botones de adquirir y asi y pues no xd*/}
-          </Modal.Body>
-          <Modal.Footer>
-          <Button 
-              variant="primary" 
-              onClick={() => Swal.fire({
-                  title: 'Nivel de membresía modificado',
-                  icon: 'success',
-                  confirmButtonText: 'Aceptar'
-              })}
-            >
-              Cambiar membresía
-            </Button>
-            <Button onClick={onHide}>Cancelar</Button>
-          </Modal.Footer>
-        </Modal>
-    );
-}
 
 function ClienteMembresias(){
     const urlClientes = "http://localhost:8080/api/power/cliente/";
@@ -108,19 +25,27 @@ function ClienteMembresias(){
     const [ num_telefonico, setNum ] = useState("");
     const [ correo, setCorreo ] = useState("");
     const [ membresia, setMembresia ] = useState([]);
+    const [ membresias, setMembresias ] = useState([]);
     const [ contra, setContra ] = useState("");
     const [ num_tarjeta, setNumTarjeta ] = useState("");
     const [ cvv, setCVV ] = useState(0);
     const [estatus, setEstatus]= useState(true);
     const [rol, setRol]= useState("");
     const [telefono, setTelefono]= useState("");
-    const [identUsuario, setIdentUsuario] = useState("")
+    const [identUsuario, setIdentUsuario] = useState("");
     //Membresia
+
     const [cliente, setClientes]= useState([]);
     const [precio, setPrecio]=useState("");
     const [tipoMembresia, setTipoMembresia]= useState("");
     const [idMembresia, setIdMembresia]=useState("");
-    
+    const [tipoTarjeta, setTipoTarjeta]= useState("");
+    const [fecha_venc, setFecha_venc]= useState("");
+
+    function closeModal() {
+        setModalShow(false);
+    }
+
     useEffect(() => {
       getCliente();
       getMembresia();
@@ -140,7 +65,8 @@ function ClienteMembresias(){
         url: urlMembresias 
         });
 
-        setMembresias(respuesta.data.data); 
+        setMembresias(respuesta.data.data);
+        setMembresiasUser(respuesta.data.data); 
     }
     
     const setCliente = async (usuario) => {
@@ -162,7 +88,7 @@ function ClienteMembresias(){
         }
     }
 
-    const setMembresias = async (membresia) => {
+    const setMembresiasUser = async (membresia) => {
 
         for (let i = 0; i < membresia.length; i++) {
             const element = membresia[i];
@@ -177,6 +103,66 @@ function ClienteMembresias(){
             }
         }
     } 
+
+    const validar = (event) => {
+        event.preventDefault();
+
+        if (!membresia) {
+            Swal.fire("Campo Membresía vacío", "Selecciona un tipo de membresía", "warning");
+            return;
+        }
+        if ((!cvv || cvv==="") || cvv.length < 3) {
+            Swal.fire("Campo CVV inválido", "Escribe un CVV válido de 3 digitos", "warning");
+            return;
+        }
+        if (!tipoTarjeta || tipoTarjeta.trim() === "") {
+            Swal.fire("Campo Tipo de tarjeta vacío", "Seleccione el tipo de tarjeta", "warning");
+            return;
+        }
+        if (!fecha_venc || fecha_venc.trim() === "") {
+            Swal.fire("Campo Fecha de vencimiento inválido", "Escribe la fecha de vencimiento", "warning");
+            return;
+        }
+        const parametros = {
+            nombre: nombre,
+            cotrasenia: contra,
+            correo: correo,
+            identificadorusuario: identUsuario,
+            rol: rol,
+            telefono: num_telefonico,
+            estatus: estatus,
+            cvv: cvv,
+            numero_tarjeta: num_tarjeta,
+            adquisicion: "",
+            vencimiento: fecha_venc,
+            membresia: {
+                id: parseInt(membresia, 10)
+            }
+        };
+    
+        enviarSolicitud(parametros, urlClientes, idCliente);
+    };
+
+    const enviarSolicitud = async(parametros, url,id) => {
+        console.log(parametros)
+        console.log(parametros.membresia)
+        await axios({
+            method: 'PUT',
+            url: url+id,
+            data: parametros
+        }).then(async (result) =>{
+            console.log(result)
+            closeModal();
+            if(result.data.data == "OK"){
+                Swal.fire("¡Membresía modificada!","El membresía ha sido actualziada", "success");         
+            }
+            getMembresia()
+        })
+        .catch(function (error) {
+            Swal.fire("Error en la Solicitud", "error");
+        });
+    }
+
     return(
         
         <>
@@ -202,11 +188,6 @@ function ClienteMembresias(){
                     </>                    
                 } />
                 
-                <ModalMejorar
-                            show={modalShow}
-                            onHide={() => setModalShow(false)}
-                            datosCliente={cliente} datosMembresia={membresia}
-                />
                 <div className='mt-4 d-flex justify-content-center'>
                     <Table className='table-separated-columns' style={{ width: '80%' }}>
                         <thead>
@@ -267,6 +248,97 @@ function ClienteMembresias(){
             </div>
         
         </div>
+
+            <Modal
+              show={modalShow}
+              onHide={closeModal}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                  Cambiar nivel de membresía
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h5 className="ms-2 ps-1">Nivel actual de membresía: {tipoMembresia}</h5>
+                <Container>
+                  <Row className="d-flex justify-content-center mt-3">
+                    <Col md={12}>
+                          <Form>
+                              <Form.Group className="mb-3">
+                                  <Form.Label className="fw-bold" >Número de cuenta:</Form.Label><br/>
+                                  <Form.Label>{num_tarjeta}</Form.Label>
+                                  
+                              </Form.Group>
+                          </Form>
+                      </Col>
+                  </Row>
+                  <Row className="d-flex justify-content-center">
+                      <Col md={6}>
+                          <Form>
+                              <Form.Group className="mb-3">
+                                  <Form.Label className="fw-bold">Tipo de membresía:</Form.Label>
+                                  <Form.Select required onChange={(e) => setMembresia(e.target.value)} defaultValue={idMembresia}>
+                                    <option id="selected">Selecciona una membresia</option>
+                                    {membresias.map((membresia => (
+                                        <option key={membresia.id} value={membresia.id}>{membresia.tipo_membresia}</option>
+                                    )))}
+                                </Form.Select>
+                                  
+                              </Form.Group>
+    
+                              <Form.Group className="mb-3">
+                                  <Form.Label className="fw-bold">Tipo de tarjeta:</Form.Label>
+                                  <Form.Select required onChange={(e) => setTipoTarjeta(e.target.value)}>
+                                        <option value="">Selecciona tu banco</option>
+                                        <option value="BBVA">BBVA</option>
+                                        <option value="Banamex">Banamex</option>
+                                        <option value="Banorte">Banorte</option>
+                                        <option value="Santander">Santander</option>
+                                        <option value="HSBC">HSBC</option>
+                                        <option value="Scotiabank">Scotiabank</option>
+                                        <option value="Inbursa">Inbursa</option>
+                                        <option value="Banco Azteca">Banco Azteca</option>
+                                        <option value="BanCoppel">BanCoppel</option>
+                                        <option value="Afirme">Afirme</option>
+                                        <option value="Banco del Bienestar">Banco del Bienestar</option>
+                                    </Form.Select>
+                              </Form.Group>
+                          </Form>
+                      </Col>
+    
+                      <Col md={6}>
+                          <Form>
+                              <Form.Group className="mb-3">
+                                  <Form.Label className="fw-bold">CVV:</Form.Label>
+                                  <Form.Control type="number" inputMode="number" placeholder="123"
+                                  onChange={(e) => setCVV(e.target.value)} required
+                                  onInput={(e) => { e.target.value = e.target.value.slice(0, 3);
+                                    if (e.target.value < 0) e.target.value = "";
+                                    }}/>
+                              </Form.Group>
+    
+                              <Form.Group className="mb-3">
+                                  <Form.Label className="fw-bold">Fecha de vencimiento:</Form.Label><br/>
+                                  <Form.Control type="date" onChange={(e) => setFecha_venc(e.target.value)} required/>
+                              </Form.Group>
+                          </Form>
+                      </Col>
+                    </Row>
+    
+                </Container>
+              </Modal.Body>
+              <Modal.Footer>
+              <Button onClick={closeModal} variant="outline-secondary">Cancelar</Button>
+              <Button variant="success" onClick={(e) => validar(e)}>
+                  Cambiar membresía
+                </Button>
+                
+              </Modal.Footer>
+            </Modal>
+
         </>
     )
 }
